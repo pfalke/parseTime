025d4a52fc5d0db3065413822d162888c8ee5893
Philipp <pfalkenh@gmail.com>
2014-05-24 23:59:03 +0200

    write spec

---
+++ parseTime.py
@@ -0,0 +1,37 @@
+"""
+Convert a string to a python datetime.
+
+Below are sample inputs that should work once everyting is done.
+
+- absolute dates
+wednesday
+wed
+may10
+tomorrow
+
+- absolute datetimes
+wednesday9am
+tomorrow9am
+9am
+
+- relative
+1h
+1hour
+2hours
+1d
+1day
+2days
+1w
+1week
+1weeks
+1m
+1month
+1months
+1year
+1y
+1years
+- any combination of the above
+
+"""
+
+
